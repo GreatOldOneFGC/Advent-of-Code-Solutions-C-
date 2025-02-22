4224606e69f649182cc9556fdd0989f78d3902e5
GreatOldOne <john@darkrose.co.uk>
2025-02-22 14:01:13 +0000

    2015 Day 1

---
+++ 2015day01/Program.cs
@@ -0,0 +1,30 @@
+﻿var input = string.Join('_', args);
+int currentFloor = 0;
+bool hasEnteredBasement = false;
+int currentPosition = 0;
+int finalPosition = 0;
+
+foreach (var character in input)
+{
+    switch (character)
+    {
+        case '(':
+            currentPosition++;
+            currentFloor++;
+            break;
+        case ')':
+            currentPosition++;
+            currentFloor--;
+            if (!hasEnteredBasement && currentFloor < 0)
+            {
+                finalPosition = currentPosition;
+                hasEnteredBasement = true;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+Console.WriteLine($"Part One Solution: {currentFloor}");
+Console.WriteLine($"Part Two Solution: {finalPosition}");
